ccf629c4bd6cdfa97033613e668e5a1ea9097103
Mark Moissette <kaosat.dev@gmail.com>
2013-08-19 10:27:54 +0200

    - first pass refactoring, some regretion (colors), but various elements' parsing should be more clear  * better parsing of color, vertex position and normals  * considering using buffer geometry for more effciency

--- src/amf-loader.js
+++ src/amf-loader.js
@@ -71,48 +71,9 @@ THREE.AMFLoader.prototype.load = function (url, callback) {
 
 THREE.AMFLoader.prototype.parse = function (data) {
 
-	function extractMetaData( node )
-	{
-		var metadataList = node.getElementsByTagName("metadata");
-		var result = {};
-		for (var i=0; i<metadataList.length;i++)
-		{
-			var current = metadataList[i];
-			if (current.parentNode == node)
-			{
-				var name = current.attributes.getNamedItem("type").nodeValue;
-				var value = current.textContent;
-				result[name] = value;
-			}
-		}
-		return result;
-	}
-	function parseTagText( node , name, toType , defaultValue)
-	{
-		defaultValue = defaultValue || null;
-		
-		var value = node.getElementsByTagName(name)[0].textContent;
+	//big question : should we be using something more like the COLLADA loader
 
-		if( value !== null && value !== undefined )
-		{
-			switch(toType)
-			{
-				case "float":
-					value = parseFloat(value);
-				break;
-			
-				case "int":
-					value = parseInt(value);
-				//default:
-
-			}
-		}
-		else if (defaultValue !== null)
-		{
-			value = defaultValue;
-		}
-		return value;
-	}
+	
 
 	var xmlDoc = this._parseXML( data );
 	var root = xmlDoc.documentElement;
@@ -121,15 +82,77 @@ THREE.AMFLoader.prototype.parse = function (data) {
 		throw("Unvalid AMF document, should have a root node called 'amf'");
 	}
 
-	var objectsData = root.getElementsByTagName("object"); 
-	var rootObj = new THREE.Object3D(); //storage of actual objects /meshes
-	var objectsHash = {}; //temporary storage of instances helper for amf
+	
+	return this._parseObjects(root);
+	
+};
 
+THREE.AMFLoader.prototype._parseObjects = function ( root ){
+
+	var objectsData = root.getElementsByTagName("object");
+	var scene = new THREE.Object3D(); //storage of actual objects /meshes
+	
+	var textures = this._parseTextures( root );
+	var materials = this._parseMaterials( root ); 
+	
 	for (var i=0; i< objectsData.length ; i++)
 	{
-		var currentGeometry = new THREE.Geometry();		
+		var objectData = objectsData[i];
+		
+		
+		var currentGeometry = this._parseGeometries(objectData);
+		var volumes = this._parseVolumes(objectData, currentGeometry);
+		
+		
+		///////////post process
+		var volumeColor = new THREE.Color("#ffffff");
+		var color = volumeColor !== null ? volumeColor : new THREE.Color("#ffffff");
 
-		var object = objectsData[i];
+		console.log("color", color);
+		var currentMaterial = new THREE.MeshLambertMaterial(
+		{ 	color: color,
+			//vertexColors: THREE.VertexColors,
+			 shading: THREE.SmoothShading
+			//vertexColors: THREE.FaceColors
+		} ); //TODO: only do this if no material/texture was specified
+		
+		//currentMaterial = new THREE.MeshNormalMaterial();
+		var currentMesh = new THREE.Mesh(currentGeometry, currentMaterial);
+	
+		//TODO: only do this, if no normals were specified
+		currentGeometry.computeFaceNormals();
+		currentGeometry.computeVertexNormals();
+	
+		//add additional data to mesh
+		var metadata = parseMetaData( objectData );
+		console.log("meta", metadata);
+		//add meta data to mesh
+		if('name' in metadata)
+		{
+			currentMesh.name = metadata.name;
+		}
+		
+		//TODO: if there is constellation data, don't just add meshes to the scene, but use 
+		//the info from constellation to do so (additional transforms)
+		scene.add( currentMesh );
+	}
+	
+	console.log("import result",scene)
+	return scene;
+}
+
+
+THREE.AMFLoader.prototype._parseGeometries = function (object){
+	//get geometry data
+	
+	var attributes = {};
+	
+	attributes["position"] = [];
+	attributes["normal"] = [];
+	attributes["color"] = [];
+	 
+	var objectsHash = {}; //temporary storage of instances helper for amf
+		var currentGeometry = new THREE.Geometry();		
 
 		var id = object.attributes.getNamedItem("id").nodeValue;
 		console.log("object id",id);
@@ -141,58 +164,76 @@ THREE.AMFLoader.prototype.parse = function (data) {
 		{
 			var vertice = verticesData[j];
 			var vertexList = vertice.getElementsByTagName("vertex");
-
 			for (var u=0; u<vertexList.length;u++)
 			{
 				var vertexData = vertexList[u];
 
-				//get vertex position data
-				var coordinates = vertexData.getElementsByTagName("coordinates")[0];
+				//get vertex data
+				var vertexCoords = parseCoords( vertexData );
+				var vertexNormals = parseNormals( vertexData );
+				var vertexColor = parseColor( vertexData );
 				
-				var x = parseTagText( coordinates , "x", "float")
-				var y = parseTagText( coordinates , "y", "float")
-				var z = parseTagText( coordinates , "z", "float")
-				console.log("x,y,z,",x,y,z);
-				var vertex = new THREE.Vector3(x,y,z);
-
-				//get vertex normal data (optional)
-				var normals = vertexData.getElementsByTagName("normals")[0];
-				if (normals !== null && normals !== undefined)
-				{
-					var x = normals.getElementsByTagName("x")[0].textContent;
-					x = parseFloat(x);
-					var y = normals.getElementsByTagName("y")[0].textContent;
-					y = parseFloat(y);
-					var z = normals.getElementsByTagName("z")[0].textContent;
-					z = parseFloat(z);
-					console.log("NORMALS x,y,z,",x,y,z);
-					var normal = new THREE.Vector3(x,y,z);
-				}
+				attributes["position"].push(vertexCoords);
+				attributes["normal"].push(vertexNormals);
+				attributes["color"].push(vertexColor);
 				
-				currentGeometry.vertices.push(vertex);
+				currentGeometry.vertices.push(vertexCoords);
 			}
-		}
 
-		//get volume data
+			//get edges data , if any
+			/* meh, kinda ugly spec to say the least
+			var edgesList = vertice.getElementsByTagName("edge");
+			for (var u=0; u<edgesList.length;u++)
+			{
+				var edgeData = edgesList[u];
+			}*/
+			
+		}
+		
+		
+	
+	return currentGeometry;
+}
+
+
+THREE.AMFLoader.prototype._parseVolumes = function (meshData, currentGeometry){
+//get volume data
 		var volumeData = meshData.getElementsByTagName("volume")[0]; 
-		var colorData = meshData.getElementsByTagName("color");
+		//var colorData = meshData.getElementsByTagName("color");
+		var volumeColor = parseColor(volumeData);
+		console.log("VolumeColor", volumeColor);
 		
 		var trianglesList = volumeData.getElementsByTagName("triangle"); 
-		for (var j=0;j<trianglesList.length;j++)
+		for (var j=0; j<trianglesList.length; j++)
 		{
 			var triangle = trianglesList[j];
 
-			var v1 = triangle.getElementsByTagName("v1")[0].textContent;
-			v1 = parseInt(v1);
-			var v2 = triangle.getElementsByTagName("v2")[0].textContent;
-			v2 = parseInt(v2);
-			var v3 = triangle.getElementsByTagName("v3")[0].textContent;
-			v3 = parseInt(v3);
-			console.log("v1,v2,v3,",v1,v2,v3);
+			//parse indices
+			var v1 = parseTagText( triangle , "v1", "int");
+			var v2 = parseTagText( triangle , "v2", "int");
+			var v3 = parseTagText( triangle , "v3", "int");
+			//console.log("v1,v2,v3,",v1,v2,v3);
 			
 			//add vertex indices to current geometry
 			//THREE.Face3 = function ( a, b, c, normal, color, materialIndex )
-			currentGeometry.faces.push( new THREE.Face3( v1, v2, v3 ) );
+			//var faceColor = colorData
+			
+			var face = new THREE.Face3( v1, v2, v3 ) 
+			currentGeometry.faces.push( face );
+
+			var faceColor = parseColor(triangle);
+			console.log("faceColor", faceColor);
+
+			//triangle/face coloring
+			if (faceColor !== null)
+			{
+				for( var v = 0; v < 3; v++ )  
+				{
+				    // initialize color variable
+				    face.vertexColors[ v ] = faceColor;
+				}
+			}
+			//console.log(face);
 
 			//get vertex UVs (optional)
 			var mapping = triangle.getElementsByTagName("map")[0];
@@ -207,28 +248,13 @@ THREE.AMFLoader.prototype.parse = function (data) {
 
 				//geometry.faceVertexUvs[ 0 ].push( uvCopy )
 			}
-
-
 		}
-		var currentMaterial = new THREE.MeshLambertMaterial( { color: 0x000000} ); //TODO: only do this if no material/texture was specified
-		currentMaterial = new THREE.MeshNormalMaterial();
-		var currentMesh = new THREE.Mesh(currentGeometry, currentMaterial);
-		//TODO: only do this, if no normals were specified
-		currentGeometry.computeFaceNormals();
+}
 
-		//add additional data to mesh
-		var metadata = extractMetaData(object);//fix this, should go only one level down
-		console.log("meta", metadata);
-		//add meta data to mesh
-		if('name' in metadata)
-		{
-			currentMesh.name = metadata.name;
-		}
 
-		rootObj.add( currentMesh );
-
-	}
-	var texturesData = root.getElementsByTagName("texture");
+THREE.AMFLoader.prototype._parseTextures = function ( node ){
+	//get textures data
+	var texturesData = node.getElementsByTagName("texture");
 	var textures = {};
 	if (texturesData !== undefined)
 	{
@@ -250,12 +276,16 @@ THREE.AMFLoader.prototype.parse = function (data) {
 			var textureTiling= textureData.attributes.getNamedItem("tiled").nodeValue
 			textures[textureId] = texture;
 		}
-
 	}
+}
 
-	//, map:texture
+THREE.AMFLoader.prototype._parseMaterials = function ( node ){
+	node.getElementsByTagName("material");
+}
 
-	var materialsData = root.getElementsByTagName("material");
+
+THREE.AMFLoader.prototype._parseConstellation = function (){
+	//parse constellation / scene data
 	var constellationData = root.getElementsByTagName("constellation"); 
 
 	if (constellationData !== undefined)
@@ -295,11 +325,7 @@ THREE.AMFLoader.prototype.parse = function (data) {
 		}
 	}
 
-	console.log( rootObj);
-	return rootObj;
-};
-
-
+}
 
 THREE.AMFLoader.prototype._parseXML = function (xmlStr) {
 
@@ -326,216 +352,106 @@ THREE.AMFLoader.prototype._parseXML = function (xmlStr) {
 
 }
 
-THREE.AMFLoader.prototype.parseASCII = function (data) {
-
-	var geometry, length, normal, patternFace, patternNormal, patternVertex, result, text;
-	geometry = new THREE.Geometry();
-	patternFace = /facet([\s\S]*?)endfacet/g;
-
-	while (((result = patternFace.exec(data)) != null)) {
-
-		text = result[0];
-		patternNormal = /normal[\s]+([\-+]?[0-9]+\.?[0-9]*([eE][\-+]?[0-9]+)?)+[\s]+([\-+]?[0-9]*\.?[0-9]+([eE][\-+]?[0-9]+)?)+[\s]+([\-+]?[0-9]*\.?[0-9]+([eE][\-+]?[0-9]+)?)+/g;
-
-		while (((result = patternNormal.exec(text)) != null)) {
-
-			normal = new THREE.Vector3(parseFloat(result[1]), parseFloat(result[3]), parseFloat(result[5]));
-
-		}
-
-		patternVertex = /vertex[\s]+([\-+]?[0-9]+\.?[0-9]*([eE][\-+]?[0-9]+)?)+[\s]+([\-+]?[0-9]*\.?[0-9]+([eE][\-+]?[0-9]+)?)+[\s]+([\-+]?[0-9]*\.?[0-9]+([eE][\-+]?[0-9]+)?)+/g;
-
-		while (((result = patternVertex.exec(text)) != null)) {
-
-			geometry.vertices.push(new THREE.Vector3(parseFloat(result[1]), parseFloat(result[3]), parseFloat(result[5])));
-
-		}
-
-		length = geometry.vertices.length;
-		geometry.faces.push(new THREE.Face3(length - 3, length - 2, length - 1, normal));
-
-	}
-
-	geometry.computeCentroids();
-	geometry.computeBoundingBox();
-	geometry.computeBoundingSphere();
-
-	return geometry;
-
-};
-
 THREE.EventDispatcher.prototype.apply( THREE.AMFLoader.prototype );
 
-if ( typeof DataView === 'undefined'){
-
-	DataView = function(buffer, byteOffset, byteLength){
-
-		this.buffer = buffer;
-		this.byteOffset = byteOffset || 0;
-		this.byteLength = byteLength || buffer.byteLength || buffer.length;
-		this._isString = typeof buffer === "string";
-
+function parseMetaData( node )
+	{
+		var metadataList = node.getElementsByTagName("metadata");
+		var result = {};
+		for (var i=0; i<metadataList.length;i++)
+		{
+			var current = metadataList[i];
+			if (current.parentNode == node)
+			{
+				var name = current.attributes.getNamedItem("type").nodeValue;
+				var value = current.textContent;
+				result[name] = value;
+			}
+		}
+		return result;
 	}
 
-	DataView.prototype = {
+	function parseTagText( node , name, toType , defaultValue)
+	{
+		defaultValue = defaultValue || null;
+		
+		var value = node.getElementsByTagName(name)[0].textContent;
 
-		_getCharCodes:function(buffer,start,length){
-			start = start || 0;
-			length = length || buffer.length;
-			var end = start + length;
-			var codes = [];
-			for (var i = start; i < end; i++) {
-				codes.push(buffer.charCodeAt(i) & 0xff);
-			}
-			return codes;
-		},
-
-		_getBytes: function (length, byteOffset, littleEndian) {
-
-			var result;
-
-			// Handle the lack of endianness
-			if (littleEndian === undefined) {
-
-				littleEndian = this._littleEndian;
+		if( value !== null && value !== undefined )
+		{
+			switch(toType)
+			{
+				case "float":
+					value = parseFloat(value);
+				break;
+			
+				case "int":
+					value = parseInt(value);
+				//default:
 
 			}
-
-			// Handle the lack of byteOffset
-			if (byteOffset === undefined) {
-
-				byteOffset = this.byteOffset;
-
-			} else {
-
-				byteOffset = this.byteOffset + byteOffset;
-
-			}
-
-			if (length === undefined) {
-
-				length = this.byteLength - byteOffset;
-
-			}
-
-			// Error Checking
-			if (typeof byteOffset !== 'number') {
-
-				throw new TypeError('DataView byteOffset is not a number');
-
-			}
-
-			if (length < 0 || byteOffset + length > this.byteLength) {
-
-				throw new Error('DataView length or (byteOffset+length) value is out of bounds');
-
-			}
-
-			if (this.isString){
-
-				result = this._getCharCodes(this.buffer, byteOffset, byteOffset + length);
-
-			} else {
-
-				result = this.buffer.slice(byteOffset, byteOffset + length);
-
-			}
-
-			if (!littleEndian && length > 1) {
-
-				if (!(result instanceof Array)) {
-
-					result = Array.prototype.slice.call(result);
-
-				}
-
-				result.reverse();
-			}
-
-			return result;
-
-		},
-
-		// Compatibility functions on a String Buffer
-
-		getFloat64: function (byteOffset, littleEndian) {
-
-			var b = this._getBytes(8, byteOffset, littleEndian),
-
-				sign = 1 - (2 * (b[7] >> 7)),
-				exponent = ((((b[7] << 1) & 0xff) << 3) | (b[6] >> 4)) - ((1 << 10) - 1),
-
-			// Binary operators such as | and << operate on 32 bit values, using + and Math.pow(2) instead
-				mantissa = ((b[6] & 0x0f) * Math.pow(2, 48)) + (b[5] * Math.pow(2, 40)) + (b[4] * Math.pow(2, 32)) +
-							(b[3] * Math.pow(2, 24)) + (b[2] * Math.pow(2, 16)) + (b[1] * Math.pow(2, 8)) + b[0];
-
-			if (exponent === 1024) {
-				if (mantissa !== 0) {
-					return NaN;
-				} else {
-					return sign * Infinity;
-				}
-			}
-
-			if (exponent === -1023) { // Denormalized
-				return sign * mantissa * Math.pow(2, -1022 - 52);
-			}
-
-			return sign * (1 + mantissa * Math.pow(2, -52)) * Math.pow(2, exponent);
-
-		},
-
-		getFloat32: function (byteOffset, littleEndian) {
-
-			var b = this._getBytes(4, byteOffset, littleEndian),
-
-				sign = 1 - (2 * (b[3] >> 7)),
-				exponent = (((b[3] << 1) & 0xff) | (b[2] >> 7)) - 127,
-				mantissa = ((b[2] & 0x7f) << 16) | (b[1] << 8) | b[0];
-
-			if (exponent === 128) {
-				if (mantissa !== 0) {
-					return NaN;
-				} else {
-					return sign * Infinity;
-				}
-			}
-
-			if (exponent === -127) { // Denormalized
-				return sign * mantissa * Math.pow(2, -126 - 23);
-			}
-
-			return sign * (1 + mantissa * Math.pow(2, -23)) * Math.pow(2, exponent);
-		},
-
-		getInt32: function (byteOffset, littleEndian) {
-			var b = this._getBytes(4, byteOffset, littleEndian);
-			return (b[3] << 24) | (b[2] << 16) | (b[1] << 8) | b[0];
-		},
-
-		getUint32: function (byteOffset, littleEndian) {
-			return this.getInt32(byteOffset, littleEndian) >>> 0;
-		},
-
-		getInt16: function (byteOffset, littleEndian) {
-			return (this.getUint16(byteOffset, littleEndian) << 16) >> 16;
-		},
-
-		getUint16: function (byteOffset, littleEndian) {
-			var b = this._getBytes(2, byteOffset, littleEndian);
-			return (b[1] << 8) | b[0];
-		},
-
-		getInt8: function (byteOffset) {
-			return (this.getUint8(byteOffset) << 24) >> 24;
-		},
-
-		getUint8: function (byteOffset) {
-			return this._getBytes(1, byteOffset)[0];
 		}
+		else if (defaultValue !== null)
+		{
+			value = defaultValue;
+		}
+		return value;
+	}
 
-	 };
-}
+	function parseColor( node )
+	{
+		var colorNode = node.getElementsByTagName("color")[0];
+		var color = null; //new THREE.Color().setRGB( 0, 0, 0 );
+		
+		if (colorNode !== null && colorNode !== undefined)
+		{
+			if (colorNode.parentNode == node)
+			{
+			var r = parseTagText( node , "r", "float");
+			var g = parseTagText( node , "g", "float");
+			var b = parseTagText( node , "b", "float");
+			var color = new THREE.Color().setRGB( r, g, b );
+			}
+		}	
+		return color;
+	}
 
+
+	function parseCoords( node )
+	{
+		var coordinatesNode = node.getElementsByTagName("coordinates")[0];
+		var coords = null;
+		
+		if (coordinatesNode !== null && coordinatesNode !== undefined)
+		{
+			if (coordinatesNode.parentNode == node)
+			{
+			var x = parseTagText( node , "x", "float");
+			var y = parseTagText( node , "y", "float");
+			var z = parseTagText( node , "z", "float");
+			var coords = new THREE.Vector3(x,y,z);
+			}
+		}	
+		return coords;
+	}
+	
+	function parseNormals( node )
+	{
+		//get vertex normal data (optional)
+		var normalsNode = node.getElementsByTagName("normal")[0];
+		var normals = null;
+		
+		if (normalsNode !== null && normalsNode !== undefined)
+		{
+			if (normalsNode.parentNode == node)
+			{
+			var x = parseTagText( node , "x", "float");
+			var y = parseTagText( node , "y", "float");
+			var z = parseTagText( node , "z", "float");
+			var normals = new THREE.Vector3(x,y,z);
+			}
+		}	
+		return normals;
+	}
 
 
